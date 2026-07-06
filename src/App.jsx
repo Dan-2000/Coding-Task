73067ee36c7bfff2b2ab459ca945953674e95138
Dan-2000 <81095977+Dan-2000@users.noreply.github.com>
2026-07-06 22:17:05 +0000

    app works, will clean some css up next

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,61 @@
 import {useState} from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
 import {useTests} from './hooks/useTests';
+import { getVisibleTests } from './utils/testHelpers';
+import { SearchBar } from './components/SearchBar';
+import { StatusFilter } from './components/StatusFilter';
+import { TestForm } from './components/TestForm';
+import { TestTable } from './components/TestTable';
 
 function App() {
   const { tests, addTest, updateTest, deleteTest} = useTests();
+  const [query,setQuery] = useState('');
+  const [statusFilter, setStatusFilter] = useState('all');
+  const [sortKey, setSortKey] = useState('lastModified');
+  const [sortDir, setSortDir] = useState('desc');
+  const [editingTest, setEditingTest] = useState(null);
 
+  const visibleTests = getVisibleTests(tests, {query, statusFilter, sortKey, sortDir});
+
+  function handleSort(key) {
+    if (key === sortKey) {
+      setSortDir(sortDir === 'asc' ? 'desc' : 'asc');
+    }
+    else{
+      setSortKey(key);
+      setSortDir('asc');
+    }
+  }
+
+  function handleSubmit(data) {
+    if(editingTest) { 
+      updateTest(editingTest.id, data);
+      setEditingTest(null);
+    }
+    else {
+      addTest(data.name, data.status);
+    }
+
+  }
   return (
     <div>
-      <button onClick={() => addTest('testing', 'waiting')}>Add test</button>
-      <pre>{JSON.stringify(tests, null, 2)}</pre>
-    </div>  
+      <h1> Test Management Project</h1>
+      <TestForm
+      key = {editingTest?.id ?? 'new'}
+      onSubmit={handleSubmit}
+      existingTest = {editingTest}
+      onCancel = {() => setEditingTest(null)}/>
+      <SearchBar query={query} onChange = {setQuery} />
+      <StatusFilter statusFilter={statusFilter} onChange = {setStatusFilter} />
+      <TestTable
+      tests={visibleTests}
+      sortKey={sortKey}
+      sortDir={sortDir}
+      onSort={handleSort}
+      onEdit={setEditingTest}
+      onDelete={deleteTest}
+      />
+    </div>
     );
 }
 
